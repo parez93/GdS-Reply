58c63f2e01e5a68a520be972116d392c37072b5f
Federico Parezzan <parez93@users.noreply.github.com>
2019-02-23 14:09:08 +0100

    Add demo annotation
    Add demo annotation

---
+++ Hibernate/2-Annotazioni/Demo/src/hibernate/entity/Student.java
@@ -0,0 +1,72 @@
+package hibernate.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "student")
+public class Student {
+
+    public Student(String name, String surname, String email) {
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+    }
+
+    @Id
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "first_name")
+    private String name;
+
+    @Column(name = "last_name")
+    private String surname;
+
+    @Column(name = "email")
+    private String email;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
